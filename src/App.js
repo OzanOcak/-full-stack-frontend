ffdfe1d1ed9a0633a191e797684b40111a5a42d9
OzanOcak <ozanocak@gmail.com>
2022-09-19 15:10:41 -0400

    adding new routes in features

--- src/App.js
+++ src/App.js
@@ -6,6 +6,10 @@ import Welcome from "./features/auth/Welcome";
 import NotesList from "./features/notes/NotesList";
 import UsersList from "./features/users/UsersList";
 import DashLayout from "./components/DashLayout";
+import EditUser from "./features/users/EditUser";
+import NewUserForm from "./features/users/NewUserForm";
+import EditNote from "./features/notes/EditNote";
+import NewNote from "./features/notes/NewNote";
 
 function App() {
   return (
@@ -16,12 +20,16 @@ function App() {
         <Route path="dash" element={<DashLayout />}>
           <Route index element={<Welcome />} />
 
-          <Route path="notes">
-            <Route index element={<NotesList />} />
-          </Route>
-
           <Route path="users">
             <Route index element={<UsersList />} />
+            <Route path=":id" element={<EditUser />} />
+            <Route path="new" element={<NewUserForm />} />
+          </Route>
+
+          <Route path="notes">
+            <Route index element={<NotesList />} />
+            <Route path=":id" element={<EditNote />} />
+            <Route path="new" element={<NewNote />} />
           </Route>
         </Route>
         {/* End Dash */}
