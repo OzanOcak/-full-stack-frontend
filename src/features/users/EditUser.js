d006ef5410a2163eac61ad9dd4b9428b709a9516
OzanOcak <ozanocak@gmail.com>
2022-09-19 20:13:20 -0400

    update,delete user mutations

--- src/features/users/EditUser.js
+++ src/features/users/EditUser.js
@@ -1,7 +1,16 @@
-import React from "react";
+import { useParams } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { selectUserById } from "./usersApiSlice";
+import EditUserForm from "./EditUserForm";
 
 const EditUser = () => {
-  return <div>EditUser</div>;
-};
+  const { id } = useParams(); //path
 
+  const user = useSelector((state) => selectUserById(state, id));
+  // fetch from redux instead of from db
+
+  const content = user ? <EditUserForm user={user} /> : <p>Loading...</p>;
+
+  return content;
+};
 export default EditUser;
